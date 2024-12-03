9bf90584adfdd1c76820f161c748a6d9f2a1b9d9
tirapat05 <tirapat05.05@gmail.com>
2024-12-03 14:37:10 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
+
+double deg2rad(double deg){
+    return deg * M_PI / 180.0;
+}
+
+double rad2deg(double rad){
+    return rad * 180.0 / M_PI;
+}
+
+double findXComponent(double l1, double l2, double a1, double a2){
+    return l1 * cos(a1) + l2 * cos(a2);
+}
+
+double findYComponent(double l1, double l2, double a1, double a2){
+    return l1 * sin(a1) + l2 * sin(a2);
+}
+
+double pythagoras(double a, double b){
+    return sqrt(a * a + b * b);
+}
+
+void showResult(double length, double angle){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+    cout << "Length of the resultant vector = " << length << endl;
+    cout << "Direction of the resultant vector (deg) = "<< angle << endl;
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
@@ -12,7 +39,7 @@ int main(){
     cin >> l2;
     cout << "Enter direction of the second vector (deg): ";
     cin >> a2;
-    
+
     a1 = deg2rad(a1);
     a2 = deg2rad(a2);
     xcomp = findXComponent(l1,l2,a1,a2);
